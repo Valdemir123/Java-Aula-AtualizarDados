57deb11c82aa1b9e463578a462efe020fa2dc9a6
Valdemir <vpsilva@gmail.com>
2020-06-13 17:15:16 -0300

    Update

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -13,41 +13,29 @@ import db.DB;
 public class Program {
 
 	public static void main(String[] args) {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Connection conn = null;
-		PreparedStatement st = null;
-
+		
+		Connection _conn = null;
+		PreparedStatement _st = null;
+		
 		try {
-			conn = DB.getConnection();
-			st = conn.prepareStatement("Insert into Seller " + "(Name,Email,BirthDate,BaseSalary,DepartmentId)"
-					+ " Values " + "(?,?,?,?,?)",
-					Statement.RETURN_GENERATED_KEYS);
-
-			st.setString(1, "Carl Purple");
-			st.setString(2, "Carl@Gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1955").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
-
-			int RowsAffected = st.executeUpdate();
+			_conn = DB.getConnection();
+			_st = _conn.prepareStatement("Update Seller"
+					+" SET BaseSalary=BaseSalary+?"
+					+" Where "
+					+"(DepartmentId = ?)");
 			
-			if(RowsAffected >0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int Id = rs.getInt(1);
-					System.out.println("Rows Affected:"+ RowsAffected + ", Id:"+Id);
-				}
-				
-			} else {
-				System.out.println("No Rows Affected:");
-			}
+			_st.setDouble(1, 200.0);
+			_st.setInt(2, 2);
 			
-		} catch (SQLException e) {
+			int RowsAffected = _st.executeUpdate();
+			
+			System.out.println("Done! "+RowsAffected);
+		}
+		catch (SQLException e){
 			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		} finally {
-			DB.closeStatement(st);
+		}
+		finally {
+			DB.closeStatement(_st);
 			DB.closeConnection();
 		}
 	}
